3ee8ea32cc2a613fe19a86f0fc9377b97c2776e4
mannie.exe <manpreet.mannie.singh@gmail.com>
2022-04-07 07:47:07 -0700

    Move client-side vitals script (from CRA)

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,10 +1,10 @@
 import './utils/initEnv';
 import log from './utils/logging';
+import reportWebVitals from './utils/reportWebVitals';
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import raw from 'raw.macro';
-import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 import App from './components/App';
