9caa7ded7bd6e57ba873ce8791e15525214c42f2
mannie.exe <manpreet.mannie.singh@gmail.com>
2022-04-09 05:17:38 -0700

    Dispatch login request from `client/components/Auth`

--- packages/client/src/components/Auth/index.tsx
+++ packages/client/src/components/Auth/index.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import {
+  useState,
+  useContext,
+  ChangeEventHandler,
+} from 'react';
+import {
+  AuthMessageType,
+  LoginRequestMessage,
+} from 'cai-lib';
 
 import './Auth.css';
 
-type AuthProps = {
-  children: React.ReactNode;
-};
+import { SocketsContext } from 'contexts/SocketsContext';
 
-const Auth = (props: AuthProps) => {
+import globalLog from 'utils/logging';
+const log = globalLog.child({ namespace: 'Auth' });
 
-  const children = props.children;
+const Auth = () => {
+
+  const sockets = useContext(SocketsContext);
+  const [name, setName] = useState('');
+
+  const handleNameChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNameSubmit: ChangeEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    if (name.length < 3) return; // TODO: perform validation and show error
+
+    const message: LoginRequestMessage = { name };
+
+    log.debug(`Sending login request with name: '${message.name}'...`);
+    sockets.auth.emit(AuthMessageType.LOGIN_REQUEST, JSON.stringify(message));
+  };
 
   return (
     <section id="auth">
-      {children}
+      <form onSubmit={handleNameSubmit}>
+        <input type="text" value={name} placeholder="Please select a name" onChange={handleNameChange} />
+      </form>
     </section>
   );
 }
